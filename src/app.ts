27192f8bdbb148468737be4c73c60637b4692124
Great <ezennagreat@gmail.com>
2025-11-01 16:33:39 +0100

    feat(app): started the application skeleton

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,34 @@
-import express from "express"
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
+import { authRouter } from './routes/auth.route';
+import { errorHandler } from './middlewares/error_handler.middleware';
+
+dotenv.config({
+    path: path.resolve(__dirname, '.env'),
+});
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+    })
+);
+
+// use routers
+app.use('/api/auth', authRouter);
+
+
+// error handler
+app.use(errorHandler)
+
+
+
+app.listen(PORT, () => {
+    console.log(`Blog API service started @${PORT}`)
+})
