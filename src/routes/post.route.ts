f7c54ec7f522af74bbecfe9dc443d0d2fc168cfc
Great <ezennagreat@gmail.com>
2025-11-03 16:29:14 +0100

    fix: removed unused import in post.route

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { authenticate } from '../middlewares/auth.middleware';
 import { showAllPosts, createPost, updatePost, deletePost } from "../controllers/post.controller"
 
