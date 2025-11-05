6606c915dc62455965b9842f8fbfad72c7f928c7
Great <ezennagreat@gmail.com>
2025-11-05 19:41:52 +0100

    fix: add typecheck guard and add undefined return type

--- src/utils/slug.util.ts
+++ src/utils/slug.util.ts
@@ -1,7 +1,9 @@
-export function generateSlug(title: string): string {
-    return title
-        .toLowerCase()
-        .replace(/[^a-z0-9]+/g, '-')
-        .replace(/^-+|-+$/g, '')
-        .replace(/--+/g, '-');
+export function generateSlug(title: string): string | undefined {
+    if (title && typeof title === 'string') {
+        return title
+            .toLowerCase()
+            .replace(/[^a-z0-9]+/g, '-')
+            .replace(/^-+|-+$/g, '')
+            .replace(/--+/g, '-');
+    }
 }
