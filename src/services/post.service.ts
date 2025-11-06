00c131bb1fa355e4432db1cce668b02dab2123f2
Great <ezennagreat@gmail.com>
2025-11-06 19:39:59 +0100

    feat: implemented createPost and getPosts async methods

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -27,11 +27,8 @@ export class PostService implements PostServiceType<Post> {
             return post.rows[0];
         } catch (err) {
             if (err instanceof Error) {
-                console.log(
-                    err.stack,
-                    'a post with similar title already exists'
-                );
-                throw new Error('a post with similar title already exists');
+                console.log(err.stack, 'error getting post');
+                throw new Error('error getting post');
             }
         }
     }
@@ -60,11 +57,71 @@ export class PostService implements PostServiceType<Post> {
             return post.rows;
         } catch (err) {
             if (err instanceof Error) {
-                console.log(
-                    err.stack,
-                    'error getting user posts'
-                );
+                console.log(err.stack, 'error getting user posts');
                 throw new Error('error getting user posts');
+            }
+        }
+    }
+
+    async getPosts(): Promise<Post[] | undefined> {
+        // only return published posts
+        try {
+            const posts = await this.db.query(
+                `
+                SELECT 
+                    p.id,
+                    u.id AS author_id, 
+                    u.email AS author_email,
+                    p.title, 
+                    p.slug,  
+                    p.excerpt, 
+                    p.status, 
+                    p.view_count, 
+                    p.created_at, 
+                    p.updated_at, 
+                FROM posts AS p INNER JOIN users AS u ON p.user_id = u.id WHERE p.status = $1
+            `,
+                ['published']
+            );
+
+            return posts.rows;
+        } catch (err) {
+            if (err instanceof Error) {
+                console.log(err.stack, 'error getting posts');
+                throw new Error('error getting posts');
+            }
+        }
+    }
+
+    async createPost(
+        user_id: number,
+        title: string,
+        slug: string,
+        content: string,
+        excerpt: string,
+        status: StatusEnum
+    ): Promise<Post | undefined> {
+        try {
+            // check if slug exists
+            if (!(await this.getPostBySlug(slug))) {
+                const now = new Date().toISOString();
+
+                await this.db.query(
+                    `
+                    INSERT INTO posts 
+                        (user_id, title, slug, content, excerpt, status, created_at, updated_at) 
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+                    [user_id, title, slug, content, excerpt, status, now, now]
+                );
+
+                const newPost = await this.getPostBySlug(slug);
+
+                return newPost;
+            }
+        } catch (err) {
+            if (err instanceof Error) {
+                console.log(err.stack, 'error creating post');
+                throw new Error('error creating post');
             }
         }
     }
