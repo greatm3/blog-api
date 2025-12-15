3f7d63757c6eec24c1ba012799cbe090a1818d38
Great <ezennagreat@gmail.com>
2025-12-15 19:22:52 +0100

    changed the max retries fro redis's reconnection to 5, and added a 1000ms delay between each reconnection

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -2,12 +2,24 @@ import { createClient } from 'redis';
 import dotenv from 'dotenv';
 import path from 'path';
 
+const MAX_RETRIES = 5; // the maximum number of times the redis client would try to reconnect if server is unavailable.
+
 dotenv.config({
     path: path.resolve(__dirname, '.env')
 });
 
 const redisClient = createClient({
-    url: process.env.REDIS_URL || 'redis://localhost:6379'
+    url: process.env.REDIS_URL || 'redis://localhost:6379',
+    socket: {
+        reconnectStrategy: (retries) => {
+            if (retries >= MAX_RETRIES) {
+                console.error(`Max retries (${MAX_RETRIES}) reached. Stop connection attempts.`);
+                return new Error('Max retries reached');
+            }
+
+            return 1000; // retry every 1 second.
+        }
+    }
 });
 
 redisClient.on('error', (err) => console.log('Redis Error:', err));
