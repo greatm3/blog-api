faa7c8020003eb1501f46c353e43a0c6d45039a0
Great <ezennagreat@gmail.com>
2025-12-15 21:35:36 +0100

    feat: created the cache service layer

---
+++ src/services/caching.service.ts
@@ -0,0 +1,40 @@
+import redisClient from '../config/redis'
+
+export default class CacheService {
+
+    static async get(key: string): Promise<JSON | null> {
+
+        const data = await redisClient.get(key);
+
+        return data ? JSON.parse(data) : null
+    }
+
+    static async set(key: string, value: string, ttl: number): Promise<string | null> {
+
+        const response = await redisClient.set(key, value, {
+            EX: ttl,
+            // condition: 'NX'
+        })
+
+        return response;
+
+    }
+
+    static async del(key: string): Promise<number> {
+
+        const response = await redisClient.del(key);
+
+        return response;
+
+    }
+
+    static async delPattern(pattern: string): Promise<string[]> {
+        const keys = await redisClient.keys(pattern);
+
+        if (keys.length > 0) {
+            await redisClient.del(keys)
+        }
+
+        return keys;
+    }
+}
