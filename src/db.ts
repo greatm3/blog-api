e4cbb45df1a5d307babcf021fb00e9c7caecc03a
Great <ezennagreat@gmail.com>
2025-12-17 16:50:34 +0100

    fix: added a log block to notify server on succesful DB connection

--- src/db.ts
+++ src/db.ts
@@ -11,6 +11,21 @@ const dbConfig: PoolConfig = {
     connectionString: process.env.DATABASE_URL
 }
 
-const appPool = new Pool(dbConfig) 
+const appPool = new Pool(dbConfig)
+
+let connected = false; // control flag so, to stop continous side-effect on connection
+
+appPool.on("connect", (client) => {
+    if (!connected) {
+        console.log("Database connected.");
+        connected = true;
+    }
+})
+
+appPool.on("error", (err, client) => {
+    if (err instanceof Error) {
+        console.error("Database error:", err.message);
+    }
+})
 
 export default appPool
