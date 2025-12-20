a572fe33f8980709fae646ddbc79e3eaaa05eff4
Great <ezennagreat@gmail.com>
2025-12-20 16:31:19 +0100

    style(app): format code for consistency and readability

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,31 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import path from 'path';
-import { authRouter } from './routes/auth.route';
-import { postRouter } from './routes/post.route'
-import { errorHandler } from './middlewares/error_handler.middleware';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+import { authRouter } from "./routes/auth.route";
+import { postRouter } from "./routes/post.route";
+import { errorHandler } from "./middlewares/error_handler.middleware";
 
-import { connectRedis } from './config/redis'
+import { connectRedis } from "./config/redis";
 
 dotenv.config({
-    path: path.resolve(__dirname, '.env'),
+    path: path.resolve(__dirname, ".env"),
 });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// using an IIEE because of my typescript configuration
+// using an async because of my typescript configuration
 // i can't use await in a top level, without changing the module type in tsconfig.
 (async () => {
     try {
         await connectRedis();
     } catch (err) {
         if (err instanceof Error) {
-            console.error("Redis connection error", err.message)
+            console.error("Redis connection error", err.message);
         }
     }
-})()
+})();
 
 app.use(express.json());
 app.use(
@@ -35,13 +35,12 @@ app.use(
 );
 
 // use routers
-app.use('/api/auth', authRouter);
-app.use('/api/posts', postRouter)
-
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
 
 // error handler
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log(`Blog API service started @${PORT}`)
-})
+    console.log(`Blog API service started @${PORT}`);
+});
